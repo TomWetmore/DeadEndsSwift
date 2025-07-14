581652c58063168d0d40a144a5c72c4c13e8d9da
Thomas Wetmore <ttw4@icloud.com>
2025-07-14 10:19:54 -0400

    Some refactoring and simplification in addPerson local function.

--- DeadEndsApp/PedigreeView.swift
+++ DeadEndsApp/PedigreeView.swift
@@ -3,7 +3,7 @@
 //  DeadEndsSwift
 //
 //  Created by Thomas Wetmore on 5 July 2025.
-//  Last changed on 11 July 2025.
+//  Last changed on 13 July 2025.
 //
 
 import SwiftUI
@@ -31,7 +31,6 @@ struct PedigreeView: View {
         GeometryReader { geometry in
             ZStack {
                 // Show vertical lines to separate the view into generation columns.
-                // They were originally added for debugging.
                 ForEach(0...generations, id: \.self) { gen in
                     let x = CGFloat(gen) / CGFloat(generations) * geometry.size.width
                     Path { path in
@@ -39,8 +38,10 @@ struct PedigreeView: View {
                         path.addLine(to: CGPoint(x: x, y: geometry.size.height))
                     }.stroke(Color.blue.opacity(0.5), style: StrokeStyle(lineWidth: 1, dash: [5]))
                 }
+                // Constants used in layout calculations.
                 let colwidth = geometry.size.width / CGFloat(generations)
                 let offset = colwidth / 2.0
+                // Layout each Person in the vew.
                 ForEach(layout) { node in
                     let x = node.x * geometry.size.width + offset
                     let y = node.y * geometry.size.height
@@ -64,41 +65,32 @@ struct PedigreeView: View {
     }
 }
 
-// buildPedigreeLayout lays out the persons in the pedigree in a unit square and returns information about
+// buildPedigreeLayout lays out persons in a pedigree in a unit square and returns information about
 // them in an array of PedigreeNode structures.
 func buildPedigreeLayout(from person: GedcomNode, // Root person of pedigree.
                          generations: Int, // Number of generations in pedigree.
                          recordIndex: [String: GedcomNode]) // Record index from database.
                             -> [PedigreeNode] { // Returns the array of PedigreeNodes
-
-    // Create the Array of PedigreeNodes.
     var pedigreeNodes = [PedigreeNode]()
 
-    // addPerson is a local function that recursively adds PedigreeNodes to pedigreeNodes at the coordinates in
-    // a unit square.
+    // addPerson is a local function that recursively builds an array of PedigreeNodes.
     func addPerson(_ person: GedcomNode, gen: Int, index: Int) {
         let x = CGFloat(gen) / CGFloat(generations) // Compute x coordinate in unit square.
         let y = CGFloat(2 * index + 1) / CGFloat(1 << (gen + 1)) // Compute y coordinate in unit square.
-        let id = person.key ?? UUID().uuidString // PedigreeNodes must be identifiable.
+        let id = person.key ?? UUID().uuidString // Give each PedigreeNode an identifier.
         pedigreeNodes.append(PedigreeNode(id: id, person: person, x: x, y: y))
-        // Recurse to next generation if still needed.
+        // Recurse to next generation.
         if gen < generations - 1 {
-            if let fatherKey = person.child(withTag: "FAMC")?.value,
-               let family = recordIndex[fatherKey],
-               let fatherKey = family.child(withTag: "HUSB")?.value,
-               let father = recordIndex[fatherKey] {
+            if let father = person.father(index: recordIndex) {
                 addPerson(father, gen: gen + 1, index: 2 * index)
             }
-            if let motherKey = person.child(withTag: "FAMC")?.value,
-               let family = recordIndex[motherKey],
-               let motherKey = family.child(withTag: "WIFE")?.value,
-               let mother = recordIndex[motherKey] {
+            if let mother = person.mother(index: recordIndex) {
                 addPerson(mother, gen: gen + 1, index: 2 * index + 1)
             }
         }
     }
 
-    // Start the recursion by adding the first person to the pedigreeNodes.
+    // Start layout recursion by adding the first person to pedigreeNodes.
     addPerson(person, gen: 0, index: 0)
     // showPedigreeNodes(nodes: pedigreeNodes) // TODO: Remove this DEBUG statement.
     return pedigreeNodes
